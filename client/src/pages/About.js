1566112de4d624b4cbb4e0759645bdc083bcd445
abhay6393pratap <abhaysingh8887810379@gmail.com>
2025-03-21 02:19:58 +0530

    Creating pages and adding content to them

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,10 +1,32 @@
 import React from "react";
-import Layout from "../components/Layout/Layout";
+import Layout from "./../components/Layout/Layout";
 
 const About = () => {
   return (
     <Layout>
-      <h1>About page</h1>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/about.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p className="text-justify mt-2">
+            Welcome to BlissCart-Bazaar, your one-stop destination for quality products and unbeatable deals. Our mission is simple — to make shopping easy, affordable, and enjoyable for everyone.
+            <br />
+            <br />
+            We believe in providing more than just products; we aim to deliver an exceptional shopping experience. From carefully curated collections to reliable delivery and outstanding customer support, we’re here to ensure you get what you need without the hassle.
+            <br />
+            <br />
+            Whether you're shopping for the latest trends, must-have gadgets, or everyday essentials — BlissCart-Bazaar is committed to bringing convenience and satisfaction to your doorstep.
+            <br />
+            <br />
+            Join our growing community of happy customers and experience the future of online shopping today!
+          </p>
+        </div>
+      </div>
     </Layout>
   );
 };
--- client/src/pages/Policy.js
+++ client/src/pages/Policy.js
@@ -1,10 +1,29 @@
 import React from "react";
-import Layout from "../components/Layout/Layout";
+import Layout from "./../components/Layout/Layout";
 
 const Policy = () => {
   return (
     <Layout>
-       <h1>Policy Page </h1>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/contactus.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-6" >
+          <h6>🔒 Privacy Policy</h6>
+          <p>At [Your Store Name], we value your privacy. We collect only the information necessary to fulfill your orders. Your data is securely stored and never shared with third parties without your consent. By using our website, you agree to our privacy practices designed to protect you.</p>
+          <h6>🔄 Return & Refund Policy</h6>
+          <p>
+          Your satisfaction is our priority. If you’re not happy with your purchase, you  return within 30 days of receiving your order. Items must be unused, in their original packaging, and accompanied by a receipt or proof of purchase.  Refunds are processed within 5-7 business days. </p>
+          <h6>🛠️ Terms of Service</h6>
+          <p>
+          By using our website, you agree to abide by our terms. This includes respecting our intellectual property, using our services for lawful purposes, and acknowledging that prices and product availability may change without notice. We reserve the right to refuse service to anyone for misconduct or policy violations..</p>
+
+        </div>
+      </div>
     </Layout>
   );
 };
