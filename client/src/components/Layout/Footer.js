d4a3d71b085ed059e8b4f028b3a9a37daf1cbeb4
abhay6393pratap <abhaysingh8887810379@gmail.com>
2025-03-20 23:27:17 +0530

    Creating Pages and routes

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="bg-dark text-light p-3">
+         <h4 className="text-center">
+            HII I am footer 
+         </h4>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
