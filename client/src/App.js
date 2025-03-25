bf340ab5b646cb1f1ee3f8c022d7b24e2ae75aa2
aman-7248 <127982352+aman-7248@users.noreply.github.com>
2025-03-26 02:43:29 +0530

    complete admin and user Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,11 @@ import PrivateRoute from "./components/Layout/Routes/Private";
 import ForgotPasword from "./pages/Auth/ForgotPasword";
 import AdminRoute from "./components/Layout/Routes/AdminRoute";
 import AdminDashboard from "./pages/Admin/AdminDashboard";
+import CreateCategory from "./pages/Admin/CreateCategory";
+import CreateProduct from "./pages/Admin/CreateProduct";
+import Users from "./pages/Admin/Users";
+import Orders from "./pages/user/Orders";
+import Profile from "./pages/user/Profile";
 
 //helmet is used for Search engine optimaztion (SEO)
 
@@ -25,9 +30,14 @@ function App() {
         {/*when you navigate to dashboard pahle privateroute check honge then dashboard pe navigate hoga */}
         <Route path="/dashboard" element={<PrivateRoute />}>
             <Route path="user" element={<Dashboard />} />
+            <Route path="user/orders" element={<Orders />} />
+            <Route path="user/profile" element={<Profile />} />
         </Route>
          <Route path="/dashboard" element={<AdminRoute/>}> 
               <Route path="admin" element={<AdminDashboard/>} />
+              <Route path="admin/create-category" element={<CreateCategory/>} />
+              <Route path="admin/create-product" element={<CreateProduct/>} />
+              <Route path="admin/users" element={<Users/>} />
          </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
