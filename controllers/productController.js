05d7b04d080c4856303882b9e67dcdb79c23012b
abhay6393pratap <abhaysingh8887810379@gmail.com>
2025-03-29 22:58:59 +0530

    implementing crud in Product

--- controllers/productController.js
+++ controllers/productController.js
@@ -48,3 +48,80 @@ export const createProductController=async(req,res)=>{
         })
     }
 };
+
+// get all products
+export const getProductController=async(req,res)=>{
+    try{
+        const products= await productModel.find({}).populate('category').select("-photo").limit(12).sort({createdAt:-1});
+        res.status(200).send({
+            success: true,
+            countTotal: products.length,
+            message:"All Products",
+            products,
+
+        })
+    }catch(error){
+        console.log(error)
+        res.status(500).send({
+            success:false,
+            message:"Error in getting Products",
+            error:error.message
+        })
+    }
+};
+
+// single product
+export const getSingleProductController=async(req,res)=>{
+    try{
+        const product=await productModel.findOne({slug:req.params.slug}).select("-photo").populate("category");
+        res.status(200).send({
+            success:true,
+            message:"Single Product Fetched",
+            product,
+        })
+    }catch(error){
+        console.log(error)
+        res.status(500).send({
+            success:false,
+            message:"Error while getting single product",
+            error
+        })
+    }
+};
+
+// get photo
+
+export const productPhotoController=async(req,res)=>{
+    try{
+        const product=await productModel.findById(req.params.pid).select("photo");
+        if(product.photo.data){
+            res.set('Content-type',product.photo.contentType);
+            return res.status(200).send(product.photo.data);
+        }
+    }catch(error){
+        console.log(error)
+        res.status(500).send({
+            success:false,
+            message:"Error while getting photo",
+            error
+        })
+    }
+};
+
+// delete product
+export const deleteProductController=async(req,res)=>{
+    try{
+        await productModel.findByIdAndDelete(req.params.pid).select("-photo");
+        res.status(200).send({
+            success:true,
+            message:"Product Deleted successfully",
+        })
+    }catch(error){
+        console.log(error)
+        res.status(500).send({
+            success:false,
+            message:"Error while deleting the product",
+            error
+        })
+    }
+};
